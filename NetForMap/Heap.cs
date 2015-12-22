8c4f7ac4019b3c06a553bf9977e68222066bf9d5
Olga Rabodzei <appossumik@gmail.com>
2015-12-22 22:52:21 +0200

    Rewrite heap class
    BUT didn't rewrite dependent classes!

--- NetForMap/Heap.cs
+++ NetForMap/Heap.cs
@@ -5,104 +5,49 @@ namespace NetForMap
     {
         private int count = 0;
         private int lastIndex = 0;
-        private Pair[] heap;
+        private int[] heap;
+        private Pair[] points;
 
-        public Heap(int _count, Pair first)
+        public Heap(Pair[] _points)
         {
-            this.count = _count;
-            this.heap = new Pair[_count];
-            this.heap[0] = first;
+            int pointsCount = _points.Length;
+            this.count = 2*(pointsCount-1);
+            this.heap = new int[this.count];
+            for (int i = pointsCount - 2; i > 0; i--) {
+                this.heap[i] = int.MinValue;
+            }
+            for (int i = this.count - 1; i >= pointsCount-1; i--) {
+                this.heap[i] = this.count - i;
+            }
+            BuildHeap();
+        }
+
+        private void BuildHeap() {
+            int pointsStartInHeap = points.Length - 1;
+            for (int i = count - 1; i >= pointsStartInHeap; i--) {
+                SiftUp(i);
+            }
         }
 
         private void SiftUp(int index)
         {
             if (index == 0) return;
-            if (heap[(index - 1) / 2] < heap[index])
+            if (heap[(index - 1) / 2] == int.MinValue || // If parent is empty OR
+                points[heap[(index - 1) / 2]].getConsumption() < points[heap[index]].getConsumption()) // If cur priority is greater
             {
-                Pair a = heap[index];
-                heap[index] = heap[(index - 1) / 2];
-                heap[(index - 1) / 2] = a;
-
+                heap[(index - 1) / 2] = heap[index];
                 SiftUp((index - 1) / 2);
             }
         }
 
-        private void SiftDown(int index)
-        {
-            if (index == this.count) return;
-            //if there is one child
-            int minChildIndex = 0;
-            if (2 * index + 1 < this.count)
-                minChildIndex = 2 * index + 1;
-            else
-                return;
-            //if there are two children choose max
-            if (
-                ((2 * index + 2) < this.count) &&
-                (heap[2 * index + 1] < heap[2 * index + 2])
-              )
-                minChildIndex = 2 * index + 2;
-            //compare parent and child
-            if (heap[index] < heap[minChildIndex])
-            {
-                Pair a = heap[index];
-                heap[index] = heap[minChildIndex];
-                heap[minChildIndex] = a;
-
-                SiftDown(minChildIndex);
-            }
-        }
-
-        public void Insert(Pair p)
-        {
-            if (this.lastIndex + 1 == this.count) return;
-            this.lastIndex++;
-            this.heap[lastIndex] = p;
-            SiftUp(lastIndex);
-        }
-
         public Pair GetMax()
         {
-            return this.heap[0];
+            return points[heap[0]];
         }
 
-        public void ChangePriority(double newPriority, int index = 0)
-        {
-            this.heap[index].setConsumption(newPriority);
-            if (index == 0)
-            {
-                SiftDown(0);
-            }
-            else if (index < this.count && this.heap[index / 2] < this.heap[index]) //if parent is smaller
-            { SiftUp(index); }
-            else { SiftDown(index); }
-        }
-
-        public int FindPathWithPoint(ref int heapIndex, int point, bool itFinish = true)
-        {
-            if (itFinish)
-            {
-                for (int i = 0; i < this.count; i++)
-                {
-                    if (this.heap[i].getFinish() == point)
-                    {
-                        heapIndex = i;
-                        return this.heap[i].getStart();
-                    }
-                }
-            }
-            else //it's start
-            {
-                for (int i = 0; i < this.count; i++)
-                {
-                    if (this.heap[i].getStart() == point)
-                    {
-                        heapIndex = i;
-                        return this.heap[i].getFinish();
-                    }
-                }
-            }
-            return -1;
+        public void ChangePriority(double newPriority, int index) {
+            points[index].setConsumption(newPriority);
+            SiftUp(points.Length - 1 + index);
         }
     }
 }
